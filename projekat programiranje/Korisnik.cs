d75b899b1a39f2054694cb845148c078394d5c82
Filip Grujić <fgrujic@smart.edu.rs>
2024-05-30 07:31:17 +0200

    Instaliran package za JSON editovanje, napravljen sistem čuvanja korisnika u JSON file

--- projekat programiranje/Korisnik.cs
+++ projekat programiranje/Korisnik.cs
@@ -4,29 +4,60 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.LinkLabel;
 
 namespace projekat_programiranje
 {
     public static class Korisnik
     {
         // Suma svih poena akumulirana kroz svaki kviz predjen
-        public static int totalPoeni;
+        public static int totalPoeni = 0;
         // Prosecna tacnost izrazena u decimali (70% = 0.7 itd.)
-        public static double prosecnaTacnost;
+        public static double prosecnaTacnost = 0.0;
 
-        public static int brojProdjenihKvizova;
-        public static int brojEjsovanihKvizova; // Ejsovani kviz je kviz kojeg je korisnik uradio bez greške
-        public static double razlomakEjsovanihProdjenih; // broj ejsovanih / broj prodjenih
-        public static string[] uradjeniKvizovi = new string[9999];
+        public static int brojProdjenihKvizova = 0;
+        public static int brojEjsovanihKvizova = 0; // Ejsovani kviz je kviz kojeg je korisnik uradio bez greške
+        public static double razlomakEjsovanihProdjenih = 0.0; // broj ejsovanih / broj prodjenih
+        public static Dictionary<string, int> uradjeniKvizovi = new Dictionary<string, int>();
 
         public static void UcitajKorisnika()
         {
             // TODO Gartman
         }
-         
+
+        struct data
+        {
+            public int _totalPoeni;
+            public double _prosecnaTacnost;
+            public int _brojProdjenihKvizova;
+            public int _brojEjsovanihKvizova;
+            public double _razlomakEjsovanihProdjenih;
+            public Dictionary<string, int> _uradjeniKvizovi;
+        };
+
         public static void SacuvajKorisnika()
         {
-            // TODO Grujić
+
+            data dataToSave = new data()
+            {
+                _totalPoeni = totalPoeni,
+                _prosecnaTacnost = prosecnaTacnost,
+                _brojProdjenihKvizova = brojProdjenihKvizova,
+                _brojEjsovanihKvizova = brojEjsovanihKvizova,
+                _razlomakEjsovanihProdjenih = razlomakEjsovanihProdjenih,
+                _uradjeniKvizovi = uradjeniKvizovi
+            };
+
+            string stringjson = JsonConvert.SerializeObject(dataToSave);
+            using (StreamWriter outputFile = new StreamWriter("./korisnik.json"))
+            {
+                outputFile.WriteLine(stringjson);
+            }
         }
     }
 }
